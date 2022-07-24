0ce80b33725c9ad37d6e09b5ab6d1f574d08cffc
eclipsebug <860337520@qq.com>
2022-07-24 16:36:10 +0800

    获取组织架构数据，并进行树形处理

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,29 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+// 引入多个模块的规则
+import approvalsRouter from './modules/approvals'
+import departmentsRouter from './modules/departments'
+import employeesRouter from './modules/employees'
+import permissionRouter from './modules/permission'
+import attendancesRouter from './modules/attendances'
+import salarysRouter from './modules/salarys'
+import settingRouter from './modules/setting'
+import socialRouter from './modules/social'
+
+// 动态路由
+//{path:'/approvalsRouter',children:{}}
+export const asyncRoutes = [
+	approvalsRouter,
+	departmentsRouter,
+	employeesRouter,
+	permissionRouter,
+	attendancesRouter,
+	salarysRouter,
+	settingRouter,
+	socialRouter
+]
+
 Vue.use(Router)
 
 /* Layout */
@@ -61,7 +84,8 @@ export const constantRoutes = [
 const createRouter = () => new Router({
 	// mode: 'history', // require service support
 	scrollBehavior: () => ({ y: 0 }),
-	routes: constantRoutes
+	//将动态路由和静态路由合并
+	routes: [...constantRoutes, ...asyncRoutes]
 })
 
 const router = createRouter()
--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,17 +1,79 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>
-        员工
-      </h2>
+      <el-card>
+
+        <tree-tool :tree-data="company"></tree-tool>
+      </el-card>
+      <el-tree default-expand-all :data="departs" :props="defaultProps">
+        <template v-slot="{data}">
+          <tree-tool :tree-data="data" :is-root="true"></tree-tool>
+
+        </template>
+      </el-tree>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+
+import TreeTool from '@/views/departments/components/tree-tool'
+
+import { getDepartments } from '@/api/departments'
+// import { tranListToTreeData, transListNew } from '@/utils'
+import { transListNew } from '@/utils'
+
+export default {
+
+  created() {
+    this.getDepartments()
+  },
+  components: { TreeTool },
+  data() {
+    return {
+      defaultProps: {
+        label: 'name'
+      },
+      departs: [
+        {
+          name: '总裁办', manager: '曹操',
+          children: [
+            {
+              name: '董事会', manager: '曹丕'
+            }
+          ]
+        },
+        {
+          name: '行政部', manager: '刘备'
+        },
+        {
+          name: '人事部', manager: '孙权'
+        }
+      ],
+      company: { name: '江苏传智播客教育科技股份有限公司', manager: '负责人' }
+    }
+  },
+  methods: {
+    async getDepartments() {
+      const res = await getDepartments()
+      this.company = {
+        name: res.companyName,
+        manager: '负责人' || res.companyManage
+      }
+      // this.departs = res.depts
+      // this.departs = tranListToTreeData(res.depts, '');
+      this.departs = transListNew(res.depts);
+
+      console.log(res)
+    }
+  }
+}
 </script>
 
-<style>
-
+<style scoped lang="scss">
+.app-container {
+  width: 850px;
+  margin: 10px auto;
+  border: 1px solid red;
+}
 </style>
