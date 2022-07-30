d9b4fba42eafb85782a0674de77513da6127be82
eclipsebug <860337520@qq.com>
2022-07-30 19:10:18 +0800

    实现将员工数据导出功能

--- src/views/employees/index.vue
+++ src/views/employees/index.vue
@@ -7,41 +7,41 @@
         </template>
         <template #after>
           <el-button size="mini" type="primary" @click="$router.push('/import')">导入</el-button>
-          <el-button size="mini" type="danger">导出</el-button>
+          <el-button size="mini" type="danger" @click="exportToExcel">导出</el-button>
           <el-button size="mini" type="warning" @click="showDialog=true">新增员工</el-button>
         </template>
       </tool-bar>
       <el-table
-        border
-        :data="list"
-        style="width: 100%"
+          border
+          :data="list"
+          style="width: 100%"
       >
         <el-table-column
-          type="index"
-          label="序号"
+            type="index"
+            label="序号"
         />
         <el-table-column
-          prop="username"
-          label="姓名"
-          width="180"
+            prop="username"
+            label="姓名"
+            width="180"
         />
         <el-table-column
-          prop="workNumber"
-          label="工号"
+            prop="workNumber"
+            label="工号"
         />
         <el-table-column
-          prop="formOfEmployment"
-          label="聘用形式"
-          :formatter="formatter"
+            prop="formOfEmployment"
+            label="聘用形式"
+            :formatter="formatter"
         />
         <el-table-column
-          prop="departmentName"
-          label="部门名称"
+            prop="departmentName"
+            label="部门名称"
         />
         <el-table-column
-          label="入职时间"
-          sortable=""
-          align="center"
+            label="入职时间"
+            sortable=""
+            align="center"
         >
           <template slot-scope="{row}">
             {{ row.timeOfEntry | formatDate }}
@@ -50,7 +50,7 @@
         <el-table-column label="账户状态" align="center" sortable="" prop="enableState">
           <template slot-scope="{ row }">
             <!-- 根据当前状态来确定 是否打开开关 -->
-            <el-switch :value="row.enableState === 1" />
+            <el-switch :value="row.enableState === 1"/>
           </template>
         </el-table-column>
         <el-table-column label="操作" sortable="" fixed="right" width="280">
@@ -66,22 +66,33 @@
       </el-table>
       <el-row style="margin-top:20px " type="flex" justify="center">
         <el-pagination
-          background
-          layout="total, sizes, prev, pager, next, jumper"
-          :total="total"
-          :page-sizes="[1, 5, 10, 20]"
-          :current-page.sync="page.page"
-          @current-change="changeCurrent"
-          @size-change="SizeChange"
+            background
+            layout="total, sizes, prev, pager, next, jumper"
+            :total="total"
+            :page-sizes="[1, 5, 10, 20]"
+            :current-page.sync="page.page"
+            @current-change="changeCurrent"
+            @size-change="SizeChange"
         />
       </el-row>
     </div>
-    <AddEmployee :is-show-dialog.sync="showDialog" />
+    <AddEmployee :is-show-dialog.sync="showDialog"/>
   </div>
 </template>
 
 <script>
 
+import { formatDate } from '@/filters'
+
+const headers = {
+  '姓名': 'username',
+  '手机号': 'mobile',
+  '入职日期': 'timeOfEntry',
+  '聘用形式': 'formOfEmployment',
+  '转正日期': 'correctionTime',
+  '工号': 'workNumber',
+  '部门': 'departmentName'
+}
 import { delEmployee, getEmployeeList } from '@/api/employees'
 import EmployeeEnum from '@/api/constant/employees'
 import { Message } from 'element-ui'
@@ -104,6 +115,23 @@ export default {
     this.getEmployeeList()
   },
   methods: {
+    async exportToExcel() {
+      const { export_json_to_excel } = await import(
+          /*webpackChunkName:'export2Excel'*/
+          '@/utils/Export2Excel')
+      // const tHeadere = ['Id', '标题,', '作者', '阅读', '时间']
+      const { rows } = await getEmployeeList({ page: 1, size: this.total })
+      const resData = this.formatterData(rows, headers)
+      export_json_to_excel({
+        header: Object.keys(headers),
+        // data: [['13399999', '张三', '2020-2020-2020', '2020', '79119'], ['13399999', '张三', '2020-2020-2020', '2020', '79119'], ['13399999', '张三', '2020-2020-2020', '2020', '79119'], ['13399999', '张三', '2020-2020-2020', '2020', '79119'], ['13399999', '张三', '2020-2020-2020', '2020', '79119'], ['13399999', '张三', '2020-2020-2020', '2020', '79119'], ['13399999', '张三', '2020-2020-2020', '2020', '79119']],
+        data: resData,
+        filename: '员工信息表',
+        autoWidth: true,
+        bookType: 'xlsx'
+      })
+    },
+
     async getEmployeeList() {
       const { rows, total } = await getEmployeeList(this.page)
       this.list = rows
@@ -129,6 +157,32 @@ export default {
       await delEmployee(id)
       Message.success('删除成功')
       await this.getEmployeeList()
+    },
+    //数据转化 导出
+    formatterData(rows, headers) {
+      //遍历原始数据
+      return rows.map(item => {
+        //item>>员工对象
+        //返回的值是为数组
+        //顺序需要和headers的key一一对应
+        //直接map>>Object.keys(headers)
+        //可以拿到遍历每一项的中文的key
+        //中文key>>映射表>>英文的key>通过item可以指导具体数据进行返回
+
+        return Object.keys(headers).map(key => {
+          //格式化时间
+          //如果是时间 >>格式化
+          if (['timeOfEntry', 'correctionTime'].includes(headers[key])) {
+            return formatDate(item[headers[key]])
+          }
+          // 如果是聘用形式>>转化1.正式员工>>通过item可以找到具体数据进行返回
+          if (headers[key] === 'formOfEmployment') {
+            return EmployeeEnum.hireType.find(child => child.id === +child.id === +item[headers[key]])?.value || '未知'
+          }
+          return item[headers[key]]
+        })
+      })
+
     }
   }
 }
